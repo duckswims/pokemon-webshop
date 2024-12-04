b53e165c5aecc47d48c75e207457a54f64971b2a
jdai01-mm <daijiahui.de@gmail.com>
2024-12-04 18:40:30 +0100

    header: absolute path used

--- myWebShop/header.php
+++ myWebShop/header.php
@@ -8,36 +8,36 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900&display=swap"
         rel="stylesheet">
-    <link rel="stylesheet" href="styles/styles.css">
-    <link rel="stylesheet" href="styles/darkmode.css">
-    <link rel="stylesheet" href="styles/header.css">
+    <link rel="stylesheet" href="/myWebShop/styles/styles.css">
+    <link rel="stylesheet" href="/myWebShop/styles/darkmode.css">
+    <link rel="stylesheet" href="/myWebShop/styles/header.css">
 </head>
 
 <body>
     <header>
         <div class="left">
-            <a href="index.php" target="_parent">
+            <a href="/myWebShop/index.php" target="_parent">
                 <div class="logo-container">
-                    <img src="img/shop-logo.png" class="logo" alt="PokéMart">
+                    <img src="/myWebShop/img/shop-logo.png" class="logo" alt="PokéMart">
                 </div>
             </a>
         </div>
         <div class="right">
             <div class="nav-links">
-                <a href="about.php" target="_parent">About Us</a>
-                <a href="shoppingCart.php" target="_parent">Shopping Cart</a>
-                <a href="login.php" target="_parent">Login</a>
-                <a href="registration.php" target="_parent">Register</a>
-                <a href="price-calculator.php" target="_parent">Price Calculator</a>
+                <a href="/myWebShop/about.php" target="_parent">About Us</a>
+                <a href="/myWebShop/shoppingCart.php" target="_parent">Shopping Cart</a>
+                <a href="/myWebShop/login.php" target="_parent">Login</a>
+                <a href="/myWebShop/registration.php" target="_parent">Register</a>
+                <a href="/myWebShop/price-calculator.php" target="_parent">Price Calculator</a>
             </div>
             <div>
                 <button class="nav-button mode-button">
-                    <img src="img/mode/light.png" class="nav-img mode-img">
+                    <img src="/myWebShop/img/mode/light.png" class="nav-img mode-img">
                 </button>
             </div>
             <div class="hamburger-menu">
                 <button class="hamburger-btn">
-                    <img src="img/hamburger.png" alt="Menu" class="hamburger-icon">
+                    <img src="/myWebShop/img/hamburger.png" alt="Menu" class="hamburger-icon">
                 </button>
             </div>
         </div>
