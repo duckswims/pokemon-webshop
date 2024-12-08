e8fd1773429b729208636e2ac1bb79970a02e5f8
jdai01-mm <daijiahui.de@gmail.com>
2024-12-08 22:43:24 +0100

    absolute filepath removed

--- myWebShop/footer.php
+++ myWebShop/footer.php
@@ -8,9 +8,9 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900&display=swap"
         rel="stylesheet">
-    <link rel="stylesheet" href="/myWebShop/styles/styles.css">
-    <link rel="stylesheet" href="/myWebShop/styles/darkmode.css">
-    <link rel="stylesheet" href="/myWebShop/styles/footer.css">
+    <link rel="stylesheet" href="styles/styles.css">
+    <link rel="stylesheet" href="styles/darkmode.css">
+    <link rel="stylesheet" href="styles/footer.css">
 </head>
 
 <body>
--- myWebShop/header.php
+++ myWebShop/header.php
@@ -8,43 +8,51 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900&display=swap"
         rel="stylesheet">
-    <link rel="stylesheet" href="/myWebShop/styles/styles.css">
-    <link rel="stylesheet" href="/myWebShop/styles/darkmode.css">
-    <link rel="stylesheet" href="/myWebShop/styles/header.css">
+    <link rel="stylesheet" href="styles/styles.css">
+    <link rel="stylesheet" href="styles/darkmode.css">
+    <link rel="stylesheet" href="styles/header.css">
 </head>
 
 <body>
     <header>
         <div class="left">
-            <a href="/myWebShop/index.php" target="_parent">
+            <a href="index.php" target="_parent">
                 <div class="logo-container">
-                    <img src="/myWebShop/img/shop-logo.png" class="logo" alt="PokéMart">
+                    <img src="img/shop-logo.png" class="logo" alt="PokéMart">
                 </div>
             </a>
         </div>
         <div class="right">
             <div class="nav-links">
-                <a href="/myWebShop/about.php" target="_parent">About Us</a>
-                <a href="/myWebShop/login.php" target="_parent">Login</a>
-                <a href="/myWebShop/registration.php" target="_parent">Register</a>
-                <a href="/myWebShop/price-calculator.php" target="_parent">Price Calculator</a>
+                <a href="about.php" target="_parent">About Us</a>
+                <a href="login.php" target="_parent">Login</a>
+                <a href="registration.php" target="_parent">Register</a>
+                <a href="price-calculator.php" target="_parent">Price Calculator</a>
             </div>
-            <a href="/myWebShop/shoppingCart.php" target="_parent">
+            <a href="shoppingCart.php" target="_parent">
                 <div id="cart-container">
-                    <img id="cart-icon" src="/myWebShop/img/cart.png" alt="Shopping Cart" width="30px">
+                    <img id="cart-icon" src="img/cart.png" alt="Shopping Cart" width="30px">
                     <span id="cart-count" style="display: none;">0</span>
                 </div>
             </a>
+            <button class="user-button">
+                <img src="img/user.png" class="nav-img user-img">
+            </button>
+            <div class="user-dropdown">
+                <a href="login.php" target="_parent">Login</a>
+                <a href="registration.php" target="_parent">Register</a>
+            </div>
             <button class="mode-button">
-                <img src="/myWebShop/img/mode/light.png" class="nav-img mode-img">
+                <img src="img/mode/light.png" class="nav-img mode-img">
             </button>
             <button class="hamburger-btn hamburger-menu">
-                <img src="/myWebShop/img/hamburger.png" alt="Menu" class="hamburger-icon">
+                <img src="img/hamburger.png" alt="Menu" class="hamburger-icon">
             </button>
         </div>
     </header>
 
-    <script src="/myWebShop/script/style-modification.js"></script>
+    <script src="script/style-modification.js"></script>
+    <script src="script/user-button.js"></script>
 </body>
 
 </html>
